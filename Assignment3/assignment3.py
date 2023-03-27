deac88054fa3253fbc73e96bdd417417bceb3e30
Seokwon You <youseokwon1667@gmail.com>
2023-03-28 06:39:42 +0900

    Assignment3

---
+++ Assignment3/assignment3.py
@@ -0,0 +1,162 @@
+from OpenGL.GL import *
+from glfw.GLFW import *
+import glm
+import ctypes
+
+g_vertex_shader_src = '''
+#version 330 core
+
+layout (location = 0) in vec3 vin_pos; 
+layout (location = 1) in vec3 vin_color; 
+
+out vec4 vout_color;
+
+uniform float u_color;
+
+void main()
+{
+    
+    gl_Position = vec4(vin_pos.x + (0.5 - u_color) * 2, vin_pos.y, vin_pos.z, 1.0);
+    vout_color = vec4(vin_color.x - u_color, vin_color.y - u_color, vin_color.z - u_color, 1); // you can pass a vec3 and a scalar to vec4 constructor
+    
+}
+'''
+# "u_color" changes the color value of rgb and x-coordinate value of vertex
+
+g_fragment_shader_src = '''
+#version 330 core
+
+in vec4 vout_color;
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vout_color;
+}
+'''
+
+def load_shaders(vertex_shader_source, fragment_shader_source):
+    # build and compile our shader program
+    # ------------------------------------
+    
+    # vertex shader 
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER)    # create an empty shader object
+    glShaderSource(vertex_shader, vertex_shader_source) # provide shader source code
+    glCompileShader(vertex_shader)                      # compile the shader object
+    
+    # check for shader compile errors
+    success = glGetShaderiv(vertex_shader, GL_COMPILE_STATUS)
+    if (not success):
+        infoLog = glGetShaderInfoLog(vertex_shader)
+        print("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" + infoLog.decode())
+        
+    # fragment shader
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER)    # create an empty shader object
+    glShaderSource(fragment_shader, fragment_shader_source) # provide shader source code
+    glCompileShader(fragment_shader)                        # compile the shader object
+    
+    # check for shader compile errors
+    success = glGetShaderiv(fragment_shader, GL_COMPILE_STATUS)
+    if (not success):
+        infoLog = glGetShaderInfoLog(fragment_shader)
+        print("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" + infoLog.decode())
+
+    # link shaders
+    shader_program = glCreateProgram()               # create an empty program object
+    glAttachShader(shader_program, vertex_shader)    # attach the shader objects to the program object
+    glAttachShader(shader_program, fragment_shader)
+    glLinkProgram(shader_program)                    # link the program object
+
+    # check for linking errors
+    success = glGetProgramiv(shader_program, GL_LINK_STATUS)
+    if (not success):
+        infoLog = glGetProgramInfoLog(shader_program)
+        print("ERROR::SHADER::PROGRAM::LINKING_FAILED\n" + infoLog.decode())
+        
+    glDeleteShader(vertex_shader)
+    glDeleteShader(fragment_shader)
+
+    return shader_program    # return the shader program
+
+
+def key_callback(window, key, scancode, action, mods):
+    if key==GLFW_KEY_ESCAPE and action==GLFW_PRESS:
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+def main():
+    # initialize glfw
+    if not glfwInit():
+        return
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)   # OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)  # Do not allow legacy OpenGl API calls
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE) # for macOS
+
+    # create a window and OpenGL context
+    window = glfwCreateWindow(800, 800, '2019032160', None, None)
+    if not window:
+        glfwTerminate()
+        return
+    glfwMakeContextCurrent(window)
+
+    # register event callbacks
+    glfwSetKeyCallback(window, key_callback);
+
+    # load shaders
+    shader_program = load_shaders(g_vertex_shader_src, g_fragment_shader_src)
+
+    u_color_loc = glGetUniformLocation(shader_program, 'u_color')
+
+    # prepare vertex data (in main memory)
+    vertices = glm.array(glm.float32,
+        # position        # color
+        -1.0, -1.0, 0.0,  1.0, 0.0, 0.0, # left vertex
+         1.0, -1.0, 0.0,  0.0, 1.0, 0.0, # right vertex
+         0.0,  1.0, 0.0,  0.0, 0.0, 1.0, # top vertex
+    )
+
+    # create and activate VAO (vertex array object)
+    VAO = glGenVertexArrays(1)  # create a vertex array object ID and store it to VAO variable
+    glBindVertexArray(VAO)      # activate VAO
+
+    # create and activate VBO (vertex buffer object)
+    VBO = glGenBuffers(1)   # create a buffer object ID and store it to VBO variable
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)  # activate VBO as a vertex buffer object
+
+    # copy vertex data to VBO
+    glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices.ptr, GL_STATIC_DRAW) # allocate GPU memory for and copy vertex data to the currently bound vertex buffer
+
+    # configure vertex positions
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * glm.sizeof(glm.float32), None)
+    glEnableVertexAttribArray(0)
+
+    # configure vertex colors
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * glm.sizeof(glm.float32), ctypes.c_void_p(3 * glm.sizeof(glm.float32)))
+    glEnableVertexAttribArray(1)
+
+    # loop until the user closes the window
+    while not glfwWindowShouldClose(window):
+        # render
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        glUseProgram(shader_program)
+
+        t = glfwGetTime()
+        a = (1 - glm.sin(t)) * .5
+        glUniform1f(u_color_loc, a) # this value will change the color and x-coordinate
+
+        glBindVertexArray(VAO)
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+
+        # swap front and back buffers
+        glfwSwapBuffers(window)
+
+        # poll events
+        glfwPollEvents()
+
+    # terminate glfw
+    glfwTerminate()
+
+if __name__ == "__main__":
+    main()
